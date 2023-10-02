c7edc86edbebcc4d458a2487a64ce65ddcd212ea
Abas-dev <abasisaac22@gmail.com>
2023-10-02 08:21:58 +0100

    added db link

--- chromeExtension/settings.py
+++ chromeExtension/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-&zm&6%9k92fw1fvb6=8^^c04%zlx93q)tfh1302i6tuoey^+4=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chrome-extension-vgf6.onrender.com']
+#ALLOWED_HOSTS = ['chrome-extension-vgf6.onrender.com','localhost:8000/']
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -92,13 +95,16 @@ WSGI_APPLICATION = 'chromeExtension.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = { 
+    'default': dj_database_url.parse('postgres://chrome_extension_user:nyEXcQd6Kse4hxTe1gCBkiFtQLbZO4hO@dpg-ckd6qfsiibqc738gomfg-a/chrome_extension')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
